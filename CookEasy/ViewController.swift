b67fb8de57059ec22c86f5131a65c649fa142244
MauricioZac1 <109308359+MauricioZac1@users.noreply.github.com>
2023-05-26 11:41:36 -0600

    Initial Commit

---
+++ CookEasy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookEasy
+//
+//  Created by Mauricio Zarate Chula on 5/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
